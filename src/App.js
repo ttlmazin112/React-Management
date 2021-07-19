e41b48fd2519870f3fa5e2ed121783dd5a4120d9
csh <ttlmazin112@naver.com>
2021-07-19 23:04:50 +0900

    Apply Material Ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import  Paper  from '@material-ui/core/Paper';
+import  Table  from '@material-ui/core/Table';
+import  TableHead  from '@material-ui/core/TableHead';
+import  TableBody  from '@material-ui/core/TableBody';
+import  TableRow  from '@material-ui/core/TableRow';
+import  TableCell  from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles =theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
 const customers =[
   {
@@ -31,8 +59,21 @@ const customers =[
 
 class App extends Component {
   render (){
+    const {classes} = this.props;
     return(
-      <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>번호</StyledTableCell>
+              <StyledTableCell>이미지</StyledTableCell>
+              <StyledTableCell>이름</StyledTableCell>
+              <StyledTableCell>생년월일</StyledTableCell>
+              <StyledTableCell>성별</StyledTableCell>
+              <StyledTableCell>직업</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
         {
           customers.map(c => {
             return(
@@ -48,9 +89,11 @@ class App extends Component {
             )
           })
         }
-      </div>
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
